07f140090308977d10ff2ae0f89c8105d9b16234
Kacey Cleveland <kacey@arrivedhomes.com>
2023-05-06 12:18:10 -0400

    Basic hover example with injected script

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,8 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { debounce } from "../../debounce";
 
 export default function App() {
+  const hoveredRef = useRef<HTMLElement>();
   useEffect(() => {
     console.log("content view loaded");
+
+    const cb = debounce((e: MouseEvent) => {
+      const el = document.elementFromPoint(e.clientX, e.clientY) as HTMLElement;
+      if (hoveredRef.current) {
+        console.log('existing el', hoveredRef.current);
+        hoveredRef.current.style.border = null;
+      }
+      hoveredRef.current = el;
+      hoveredRef.current['data-prevify']= 'hover';
+      hoveredRef.current.style.border = '2px solid red';
+      console.log(el);
+    }, 200);
+
+    window.addEventListener('mousemove', cb);
+
+    return () => {
+      window.removeEventListener('mousemove', cb);
+    }
   }, []);
 
   return <div className="content-view">content view</div>;
