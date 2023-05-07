55a705bd86853f53ea70425250c2d88400d990e0
Kacey Cleveland <kacey@arrivedhomes.com>
2023-05-06 21:28:17 -0400

    add basic toggle for turning extension off and on on webpage

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,29 +1,48 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { debounce } from "../../debounce";
 
 export default function App() {
   const hoveredRef = useRef<HTMLElement>();
+  const [isEnabled, setIsEnabled] = useState(false);
+
   useEffect(() => {
     console.log("content view loaded");
+    function messageReceived(msg) {
+      console.log(msg);
+      // Do your work here
+        setIsEnabled(msg.commenting);
+    }
+    //for listening any message which comes from runtime
+    console.log("adding listener");
+    chrome.runtime.onMessage.addListener(messageReceived);
 
     const cb = debounce((e: MouseEvent) => {
       const el = document.elementFromPoint(e.clientX, e.clientY) as HTMLElement;
       if (hoveredRef.current) {
-        console.log('existing el', hoveredRef.current);
+        console.log("existing el", hoveredRef.current);
         hoveredRef.current.style.border = null;
       }
       hoveredRef.current = el;
-      hoveredRef.current['data-prevify']= 'hover';
-      hoveredRef.current.style.border = '2px solid red';
+      hoveredRef.current["data-prevify"] = "hover";
+      hoveredRef.current.style.border = "2px solid red";
       console.log(el);
     }, 200);
 
-    window.addEventListener('mousemove', cb);
+    if (isEnabled) {
+      window.addEventListener("mousemove", cb);
 
-    return () => {
-      window.removeEventListener('mousemove', cb);
+      return () => {
+        window.removeEventListener("mousemove", cb);
+      };
+    } else {
+      console.log('removing event listener');
+      if (hoveredRef.current) {
+        console.log("existing el", hoveredRef.current);
+        hoveredRef.current.style.border = null;
+      }
+      window.removeEventListener("mousemove", cb);
     }
-  }, []);
+  }, [isEnabled]);
 
   return <div className="content-view">content view</div>;
 }
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import logo from "@assets/img/logo.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleComments = useCallback(() => {
+    const newVal = !isEnabled;
+    //for sending a message
+    // chrome.runtime.sendMessage({ commenting: newVal }, function (response) {
+    //   console.log("response", response);
+    //   setIsEnabled(newVal);
+    // });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { commenting: newVal },
+        function (response) {
+          console.log("response", response);
+          setIsEnabled(newVal);
+        }
+      );
+    });
+  }, [isEnabled]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +32,7 @@ const Popup = () => {
         <p>
           Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
         </p>
+        <button onClick={toggleComments}>Toggle</button>
         <a
           className="App-link"
           href="https://reactjs.org"
